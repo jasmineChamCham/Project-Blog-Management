bda6f4c12694f1f616becccf0627b90d2055444d
jasmineChamCham <jasminebkdn@gmail.com>
2023-03-30 11:31:28 +0700

    add change password function 30.03.23

--- app/src/main/java/com/example/blogapp/view/ProfileActivity.java
+++ app/src/main/java/com/example/blogapp/view/ProfileActivity.java
@@ -1,23 +1,60 @@
 package com.example.blogapp.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.blogapp.R;
+import com.example.blogapp.viewmodel.BlogRepository;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+    private FirebaseUser user;
+    EditText etCurPw;
+    EditText etNewPw;
+    EditText etConfirmPw;
+    TextView tvName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Test with jasmine user
+        if (BlogRepository.mAuth == null){
+            BlogRepository.mAuth = FirebaseAuth.getInstance();
+            BlogRepository.mAuth.signInWithEmailAndPassword("jasmine@gmail.com", "jasmine123")
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Log.d("DEBUG", "login successful");
+                            } else {
+                                Log.d("DEBUG", "login failed");
+                            }
+                        }
+                    });
+        }
+        user = BlogRepository.mAuth.getCurrentUser();
+
+        tvName = findViewById(R.id.tv_name);
+        tvName.setText(user.getEmail());
+
         Button butStatistics = findViewById(R.id.but_statistics);
         Button butNewfeed = findViewById(R.id.but_newfeed);
         Button butProfile = findViewById(R.id.but_profile);
@@ -43,52 +80,84 @@ public class ProfileActivity extends AppCompatActivity {
                 AlertDialog dialog = mDialog.create();
                 dialog.setCancelable(true);
 
-                EditText etCurPw = myView.findViewById(R.id.et_cur_pw);
-                EditText etNewPw = myView.findViewById(R.id.et_new_pw);
+                etCurPw = myView.findViewById(R.id.et_cur_pw);
+                etNewPw = myView.findViewById(R.id.et_new_pw);
+                etConfirmPw = myView.findViewById(R.id.et_confirm_pw);
                 Button butChangePass = myView.findViewById(R.id.but_change_pass);
 
                 butChangePass.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         if (etCurPw.getText().toString().equals(etNewPw.getText().toString())){
-                            Toast.makeText(ProfileActivity.this, "New password is the same as the current one", Toast.LENGTH_LONG).show();
-                            etCurPw.setText("");
-                            etNewPw.setText("");
+                            Toast.makeText(ProfileActivity.this,
+                                    "New password is the same as the current one",
+                                    Toast.LENGTH_LONG).show();
+                            resetChangePasswordDialog();
+                            etCurPw.requestFocus();
+                        } else if (!etConfirmPw.getText().toString().equals(etNewPw.getText().toString())){
+                            Toast.makeText(ProfileActivity.this,
+                                    "Your confirmation password is not correct",
+                                    Toast.LENGTH_LONG).show();
+                            resetChangePasswordDialog();
                             etCurPw.requestFocus();
                         } else {
                             // CHANGE PASSWORD IN FIREBASE
-                            // ....
+                            String email = user.getEmail();
+                            String curPw = etCurPw.getText().toString();
+                            String newPw = etNewPw.getText().toString();
 
-//                    myRef.child(model.getId()).setValue(new Post(model.getId(),title,content,getRandomColor(),model.getDate(), emotion))
-//                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                @Override
-//                                public void onComplete(@NonNull Task<Void> task) {
-//                                    if (task.isSuccessful()){
-//                                        Toast.makeText(MainActivity.this, "Update note successfully", Toast.LENGTH_LONG).show();
-//                                    } else {
-//                                        Toast.makeText(MainActivity.this, "Update note failed", Toast.LENGTH_LONG).show();
-//                                    }
-//                                }
-//                            });
-
-                            dialog.dismiss();
+                            AuthCredential credential = EmailAuthProvider.getCredential(email, curPw);
+                            user.reauthenticate(credential)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()){
+                                                    user.updatePassword(newPw)
+                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<Void> taskUpdatePw) {
+                                                                    if (taskUpdatePw.isSuccessful()) {
+                                                                        Toast.makeText(ProfileActivity.this,
+                                                                                "Update password successfully",
+                                                                                Toast.LENGTH_LONG).show();
+                                                                        dialog.dismiss();
+                                                                    } else {
+                                                                        Toast.makeText(ProfileActivity.this,
+                                                                                "Failed to update password",
+                                                                                Toast.LENGTH_LONG).show();
+                                                                        resetChangePasswordDialog();
+                                                                    }
+                                                                }
+                                                            });
+                                                }
+                                                else {
+                                                    Toast.makeText(ProfileActivity.this,
+                                                            "Wrong current password",
+                                                            Toast.LENGTH_LONG).show();
+                                                    resetChangePasswordDialog();
+                                                }
+                                        }
+                                        });
                         }
                     }
                 });
-
                 dialog.show();
-
             }
         });
 
         butLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // myRef => logout
-
+                BlogRepository.mAuth.signOut();
                 Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void resetChangePasswordDialog(){
+        etCurPw.setText("");
+        etNewPw.setText("");
+        etConfirmPw.setText("");
     }
 }
