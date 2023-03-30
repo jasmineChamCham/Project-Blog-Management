1c3b8b3689ece77e6095d92ca1c6daa618950184
jasmineChamCham <jasminebkdn@gmail.com>
2023-03-30 13:13:15 +0700

    add 2 fragments to statistics activity

---
+++ app/src/main/java/com/example/blogapp/view/FollowStatisticsFragment.java
@@ -0,0 +1,36 @@
+package com.example.blogapp.view;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.blogapp.R;
+import com.example.blogapp.databinding.FragmentFollowStatisticsBinding;
+
+public class FollowStatisticsFragment extends Fragment {
+
+    private FragmentFollowStatisticsBinding binding;
+    public FollowStatisticsFragment() {
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_follow_statistics, container, false);
+    }
+}
